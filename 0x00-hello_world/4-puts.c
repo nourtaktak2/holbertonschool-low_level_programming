c00aaa2609e9f72302c98e419d12e54fd5e71767
Nour <nourtaktak280@gmail.com>
2020-02-05 21:15:44 +0100

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
-* print message 
-* return: Always 0 (success)
-*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
