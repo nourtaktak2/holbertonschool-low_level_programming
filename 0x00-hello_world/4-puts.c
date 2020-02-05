b12377094e2aef4465fb192223a3601ef223ed08
Nour <nourtaktak280@gmail.com>
2020-02-05 20:55:02 +0100

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-puts ("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/* print message */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
