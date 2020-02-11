596a2954eb7eabd1714bf1a5d8f52da8dbdc492a
Nour <nourtaktak280@gmail.com>
2020-02-11 10:54:48 +0100

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* main - Entry point
+* _putchar function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch[] = "Holberton";
+int i = 0;
+while (i <= 8)
+{
+_putchar(ch[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
