33f8f592dff68c84a413e16b028f8617617c7677
Nour <nourtaktak280@gmail.com>
2020-02-11 22:22:59 +0100

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-*print_to_98 - check the code for Holberton School students.
+* print_to_98 - check the code for Holberton School students.
 * @n : variable
 * Return: Always 0.
 */
