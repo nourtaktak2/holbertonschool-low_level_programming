4406623e4ae02f00bf7448d1a2f0917e79f5de25
Nour <nourtaktak280@gmail.com>
2020-02-13 10:31:32 +0100

    leap year

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,5 +3,7 @@
 
 void postitive_or_negative(void);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif
