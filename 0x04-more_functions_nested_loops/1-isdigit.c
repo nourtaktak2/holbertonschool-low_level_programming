dcf4c3f7f29821b5cf073b683f2ea5edab9fa1d7
Nour <nourtaktak280@gmail.com>
2020-02-13 12:05:14 +0100

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* _isdigit - check the code for a digit.
+* @c : variable
+* Return: 1 if true or 0 if false.
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
