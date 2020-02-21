331e67e0cbe94cfa377a574534c68649b930483d
Nour <nourtaktak280@gmail.com>
2020-02-21 09:33:35 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+*_strcat :concatenates two strings
+*@dest : destination
+*@src : source
+*return a pointer to the resulting string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i;
+int l;
+int k;
+char *p;
+l = 0;
+for (i = 0 ; dest[i] != 0 ; i++)
+l++;
+k = 0;
+for (k = 0 ; src[k] != 0 ; k++)
+k++;
+for (i = 0 ; i <= k ; i++)
+{
+dest[l + i] = src[i];
+}
+p = dest;
+return (p);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,6 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+char *_strcat(char *dest, char *src);
+
+#endif
