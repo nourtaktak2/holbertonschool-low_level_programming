1c95cf9b82d40540687aa3d01af1cc2dc2c6bcdf
Nour <nourtaktak280@gmail.com>
2020-03-11 00:08:52 +0100

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,17 @@
 #include "dog.h"
-
 /**
 * init_dog - main function
-* @e: parameter
-* @name:dog name 
-* @age: dog age
-* @owner: dog owner
-* Return: nothing
+* @d: parameter
+* @name: parameter
+* @age: parameter
+* @owner: parameter
+* Return: void
 */
-
-void init_dog(struct dog *e, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (e == 0)
+if (d == 0)
 return;
-(*e).name = name;
-(*e).age = age;
-(*e).owner = owner;
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
 }
