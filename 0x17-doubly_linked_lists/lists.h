8f1ee483ebbdf82aa030e0f6a0f531e895c9a9b6
Nour <nourtaktak280@gmail.com>
2020-07-08 22:29:41 +0100

    list.h

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
