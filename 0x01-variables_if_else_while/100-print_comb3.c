1e5a3168bbe070b3a318688db76aece0c1960642
Nour <nourtaktak280@gmail.com>
2020-08-02 03:52:37 +0100

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * @void: Description of parameter x
+ * Return: 0
+ */
+int main(void)
+{
+int i, j, k;
+for (i = 48 ; i <= 56; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+if (j > i)
+{
+putchar(i);
+putchar(j);
+if (i != 56 || j != 57)
+{
+for (k = 44 ; k >= 32 ; k -= 12)
+{
+putchar(k);
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
