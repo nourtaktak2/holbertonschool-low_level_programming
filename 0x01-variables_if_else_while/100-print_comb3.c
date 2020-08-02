a4e29068c6006cce81c7b7bf845aeb131205869b
Nour <nourtaktak280@gmail.com>
2020-08-02 04:00:31 +0100

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,25 @@
  */
 int main(void)
 {
-int i, j, k;
-for (i = 48 ; i <= 56; i++)
-{
-for (j = 49; j <= 57; j++)
-{
-if (j > i)
-{
-putchar(i);
-putchar(j);
-if (i != 56 || j != 57)
-{
-for (k = 44 ; k >= 32 ; k -= 12)
-{
-putchar(k);
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+  int i, j, k;
+  for (i = 48 ; i <= 56; i++)
+    {
+      for (j = 49; j <= 57; j++)
+	{
+	  if (j > i)
+	    {
+	      putchar(i);
+	      putchar(j);
+	      if (i != 56 || j != 57)
+		{
+		  for (k = 44 ; k >= 32 ; k -= 12)
+		    {
+		      putchar(k);
+		    }
+		}
+	    }
+	}
+    }
+  putchar('\n');
+  return (0);
 }
